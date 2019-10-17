0d1b46c01fd38299107c26341b20fde4cd778b30
Geon Yoon <ssy01013@gmail.com>
2019-10-17 10:29:31 -0400

    test_equal

--- triangle/test/triangle/TriangleTest.java
+++ triangle/test/triangle/TriangleTest.java
@@ -10,11 +10,52 @@ import static triangle.Triangle.Type.*;
  * Test class for the Triangle implementation.
  */
 public class TriangleTest {
-
+    // basic test
     @Test
     public void test1() {
         Type actual = Triangle.classify(10, 10, 10);
         Type expected = EQUILATERAL;
         assertEquals(actual, expected);
     }
+
+    // invalid inputs
+    @Test
+    public void test_invalid_input1() {
+        Type actual = Triangle.classify(-1, 10, 10);
+        Type expected = INVALID;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void test_invalid_input2() {
+        Type actual = Triangle.classify(10, -1, 10);
+        Type expected = INVALID;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void test_invalid_input3() {
+        Type actual = Triangle.classify(10, 10, -1);
+        Type expected = INVALID;
+        assertEquals(actual, expected);
+    }
+
+    // equal input
+    public void test_input_equal1() {
+        Type actual = Triangle.classify(10, 10, -1);
+        Type expected = INVALID;
+        assertEquals(actual, expected);
+    }
+
+    public void test_input_equal2() {
+        Type actual = Triangle.classify(10, 10, -1);
+        Type expected = INVALID;
+        assertEquals(actual, expected);
+    }
+
+    public void test_input_equal2() {
+        Type actual = Triangle.classify(10, 10, -1);
+        Type expected = INVALID;
+        assertEquals(actual, expected);
+    }
+
+
 }
